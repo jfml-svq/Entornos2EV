3a61a3189609f23f2a640e5e133b0e3f268989cf
Jose Fco <josefcomartinezledesma@gmail.com>
2021-04-01 12:19:38 +0200

    Update Aplicacion

--- src/main/java/aplicacion/Aplicacion.java
+++ src/main/java/aplicacion/Aplicacion.java
@@ -2,6 +2,6 @@ package aplicacion;
 
 public class Aplicacion {
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("hola mundo");
     }
 }
